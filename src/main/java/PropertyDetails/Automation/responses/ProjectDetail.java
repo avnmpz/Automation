98ffa09cc71be2c54fe4b6eb9423c93e08e4f8a5
avnmpz <37211400+avnmpz@users.noreply.github.com>
2025-01-30 13:51:47 +0530

    Create ProjectDetail.java

---
+++ src/main/java/PropertyDetails/Automation/responses/ProjectDetail.java
@@ -0,0 +1,55 @@
+package Property Details. Automation.responses;
+
+public class ProjectDetail {
+
+private String projectName;
+
+private String addressLine1;
+
+private String addressLine2;
+
+private String type;
+
+public String getProjectName() {
+
+return projectName;
+
+}
+
+public void setProjectName(String projectName) { this.projectName = projectName;
+
+}
+
+public String getAddressLine1() {
+
+return addressLine1;
+
+}
+
+public void setAddressLine1 (String addressLine1) { this.addressLine1 = addressLine1;
+
+}
+
+public String getAddressLine2() {
+
+return addressLine2;
+
+}
+
+public void setAddressLine2(String addressLine2) { this.addressLine2 = addressLine2;
+
+}
+
+public String getType() {
+
+return type;
+
+}
+
+public void setType(String type) {
+
+this.type = type;
+
+}
+
+}
