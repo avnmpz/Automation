85804f3dfa62da24301c7e2b7c9da671e235117f
avnmpz <37211400+avnmpz@users.noreply.github.com>
2025-01-30 13:48:32 +0530

    Create Project details interface.java

---
+++ src/main/java/PropertyDetails/Automation/interfaces/Project details interface.java
@@ -0,0 +1,11 @@
+package PropertyDetails.Automation.interfaces;
+
+import java.util.List;
+
+import Property Details. Automation.responses.ProjectDetail;
+
+public interface ProjectDeatilsInterface {
+
+public List<ProjectDetail> getProjectDeatils();
+
+}
